fa16e3cf1c7692dac17a0e86aabd23bbb0847779
jhoanMG <135554986+jhoanMG@users.noreply.github.com>
2024-06-08 08:06:37 -0500

    cambio de inicio

--- Inicio.py
+++ Inicio.py
@@ -1,44 +1,31 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 
-# Set the page title and header
-st.title("Proyecto Integrador")
-st.header("Bienvenido a nuestro Proyecto Integrador")
+# Configuración de la página
+st.set_page_config(layout="wide", page_title="Nuevas tecnologías")
 
-# Hero Section with image and project description
-st.image("https://i1.sndcdn.com/artworks-nTgGESMaN9t5TBxQ-xO5rFg-t500x500.jpg", width=600)
-st.write("*Descripción del proyecto:* Breve descripción del proyecto, sus objetivos y su impacto.")
+st.sidebar.title("Nuevas Tecnologias")
 
-# Project Overview
-st.subheader("Resumen del Proyecto")
-st.write("- Punto 1: Descripción detallada del punto 1 del proyecto.")
-st.write("- Punto 2: Descripción detallada del punto 2 del proyecto.")
-st.write("- Punto 3: Descripción detallada del punto 3 del proyecto.")
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
 
-# Features and Benefits
-st.subheader("Características y Beneficios")
-st.write("*Característica 1:* Descripción de la característica 1 y sus beneficios.")
-st.write("*Característica 2:* Descripción de la característica 2 y sus beneficios.")
-st.write("*Característica 3:* Descripción de la característica 3 y sus beneficios.")
+# Definir el título y el encabezado de la página
+st.title("Nuevas Tecnologías")
+st.header("Aquí encontrarás una variedad de datos")
 
-# Interactive Chart or Visualization (Optional)
-# Replace with your specific data and visualization
-data = [10, 20, 30, 40, 50]
-labels = ["Categoría A", "Categoría B", "Categoría C", "Categoría D", "Categoría E"]
-fig, ax = plt.subplots()
-ax.pie(data, labels=labels, autopct="%1.1f%%")
-st.pyplot(fig)
+# Agregar imagen principal
+main_image = "https://st4.depositphotos.com/10325396/25177/i/450/depositphotos_251775510-stock-photo-corridor-of-server-room-with.jpg"
+st.image(main_image, caption='Servidor de tecnología avanzada')
 
-# Call to Action
-st.subheader("¡Toma Acción!")
-st.write("*Visite nuestro sitio web:* [Enlace al sitio web del proyecto](https://example.com)")
-st.write("*Contáctenos:* [Enlace al correo electrónico de contacto](mailto:info@example.com)")
+st.subheader("Proyecto Integrador")
+st.write("- Musica")
 
-# Footer with team members and project information
-st.subheader("Equipo y Contacto")
-st.write("*Miembros del equipo:*")
-st.write("- Nombre 1: Manuela Orrego.")
-st.write("- Nombre 2: Clara Ramirez.")
-st.write("- Nombre 3: Jhoan Monsalve G.")
-st.write("*Información de contacto:*")
-st.write("Correo electrónico: [Enlace al correo electrónico de contacto](mailto:info@example.com)")
+st.subheader("Simulador Cesde")
+st.write("- Info Cesde Sede Bello")
+
+st.subheader("Proyecto Individual")
+st.write("- Intoxicacion por Sustancias Psicoactivas - Medellin")
+
+st.subheader("Elaborado Por:")
+st.write("- Clara Eliana Ramirez Lopez")
+st.write("- Jhoan Andres Monsalve Garcia")
+st.write("- Manuela Urrego")
